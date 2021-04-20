5df8967660b39aae1845dc33ba2f78b205d1fbff
Ayfri <pierre.ayfri@gmail.com>
2021-04-20 19:02:37 +0200

    feat: Add EventEmitter namespace.

--- src/utils/EventEmitter.ts
+++ src/utils/EventEmitter.ts
@@ -1,17 +1,23 @@
 import * as PIXI from 'pixi.js';
 
-type EventList = Record<string, any[]>;
+export type EventList = Record<string, any[]>;
 
 export class EventEmitter<Events extends EventList> extends PIXI.utils.EventEmitter {
-	public emit<K extends keyof Events & string>(event: K, ...args: Events[K]): boolean {
+	public emit<K extends (keyof Events) & string>(event: K, ...args: Events[K]): boolean {
 		return super.emit(event, ...args);
 	}
 
-	public on<K extends keyof Events & string>(event: K, fn: (...params: Events[K]) => void, context?: any): this {
+	public on<K extends (keyof Events) & string>(event: K, fn: (...params: Events[K]) => void, context?: any): this {
 		return super.on(event, fn as (...args: any[]) => void, context);
 	}
 
-	public once<K extends keyof Events & string>(event: K, fn: (...params: Events[K]) => void, context?: any): this {
+	public once<K extends (keyof Events) & string>(event: K, fn: (...params: Events[K]) => void, context?: any): this {
 		return super.once(event, fn as (...args: any[]) => void, context);
 	}
+}
+
+export namespace EventEmitter {
+	export type Emit<EventTypes extends EventList> = <K extends keyof EventTypes & string>(event: K, ...args: EventTypes[K]) => boolean;
+	export type On<EventTypes extends EventList, This> = <K extends keyof EventTypes & string>(event: K, fn: (...params: EventTypes[K]) => void, context?: any) => This;
+	export type Once<EventTypes extends EventList, This> = <K extends keyof EventTypes & string>(event: K, fn: (...params: EventTypes[K]) => void, context?: any) => This;
 }
