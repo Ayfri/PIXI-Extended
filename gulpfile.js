5730ba273f18c24cdf4ccf413fb6491ff3600c2d
Ayfri <pierre.ayfri@gmail.com>
2021-04-22 19:56:46 +0200

    feat: Add declarations & prepare npm scripts.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,21 @@
 const gulp = require('gulp');
 const esbuild = require('gulp-esbuild');
+const clean = require('gulp-clean');
 const {exec} = require('child_process');
 
 function bundle() {
 	return gulp
 		.src('./src/index.ts')
-		.pipe(
-			esbuild({
-				bundle: true,
-				format: 'esm',
-				keepNames: true,
-				loader: {
-					'.ts': 'ts',
-				},
-				minifySyntax: true,
-				minifyWhitespace: true,
-				outfile: 'bundle.js',
-				sourcemap: 'external',
-				target: ['chrome61', 'firefox79', 'safari11', 'edge18'],
-			})
-		)
+		.pipe(esbuild({
+			bundle: true,
+			format: 'esm',
+			keepNames: true,
+			minifySyntax: true,
+			minifyWhitespace: true,
+			outfile: 'bundle.js',
+			sourcemap: 'external',
+			target: ['chrome61', 'firefox79', 'safari11', 'edge18'],
+		}))
 		.pipe(gulp.dest('./dist/'));
 }
 
@@ -30,6 +26,11 @@ function watch() {
 
 	return gulp.watch(['src/**/*.*', 'gulpfile.js'], bundle);
 }
+
+gulp.task('clean', () => {
+	return gulp.src('dist/typings.d.ts', {read: false, allowEmpty: true}).pipe(clean());
+});
+
 
 exports.bundle = bundle;
 exports.watch = watch;
