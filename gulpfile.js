a75b1dcc7c874b124a06102a7b3adaf7b4e758e1
Ayfri <pierre.ayfri@gmail.com>
2021-04-23 16:04:42 +0200

    feat: Add publish gulp task to publish properly library.

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,25 @@ const esbuild = require('gulp-esbuild');
 const clean = require('gulp-clean');
 const {exec} = require('child_process');
 
+/**
+ *
+ * @type {Omit<Options>}
+ */
+const esbuildOptions = {
+	bundle: true,
+	format: 'cjs',
+	keepNames: true,
+	minifySyntax: true,
+	minifyWhitespace: true,
+	outfile: 'bundle.js',
+	sourcemap: 'external',
+	target: ['chrome61', 'firefox79', 'safari11', 'edge18'],
+};
+
 function bundle() {
 	return gulp
 		.src('./src/index.ts')
-		.pipe(esbuild({
-			bundle: true,
-			format: 'esm',
-			keepNames: true,
-			minifySyntax: true,
-			minifyWhitespace: true,
-			outfile: 'bundle.js',
-			sourcemap: 'external',
-			target: ['chrome61', 'firefox79', 'safari11', 'edge18'],
-		}))
+		.pipe(esbuild(esbuildOptions))
 		.pipe(gulp.dest('./dist/'));
 }
 
@@ -31,6 +37,15 @@ gulp.task('clean', () => {
 	return gulp.src('dist/typings.d.ts', {read: false, allowEmpty: true}).pipe(clean());
 });
 
+gulp.task('publish', () => {
+	esbuildOptions.target = 'node14';
+	esbuildOptions.outfile = 'index.js';
+	esbuildOptions.sourcemap = false;
+	return gulp
+		.src('./src/index.ts')
+		.pipe(esbuild(esbuildOptions))
+		.pipe(gulp.dest('./dist/'));
+});
 
 exports.bundle = bundle;
 exports.watch = watch;
