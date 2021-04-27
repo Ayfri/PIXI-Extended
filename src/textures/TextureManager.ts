5f19f0cb4f185a84149142b0e7a67252a6dd12a2
Ayfri <pierre.ayfri@gmail.com>
2021-04-27 08:33:11 +0200

    feat: Add getColorTexture function.

--- src/textures/TextureManager.ts
+++ src/textures/TextureManager.ts
@@ -1,4 +1,6 @@
 import * as PIXI from 'pixi.js';
+import {Sprite} from '../sprites';
+import {Color} from '../utils';
 
 export type TextureOrName = string | PIXI.Texture;
 export type TextureNameAndPath = [name: string, path: string];
@@ -44,4 +46,18 @@ export function getTexture(name: string): PIXI.Texture | null {
 export function getTextureOrThrow(name: string): PIXI.Texture {
 	if (loadedTexturesNames.includes(name)) return PIXI.Loader.shared.resources[name].texture!;
 	else throw new Error(`Texture '${name}' not found.`);
+}
+
+export interface ColoredTextureOptions {
+	color?: Color;
+	height?: number;
+	width?: number;
+}
+
+export function getColoredTexture(options: ColoredTextureOptions): PIXI.Texture {
+	const sprite = new Sprite(PIXI.Texture.WHITE);
+	sprite.width = options.width ?? 100;
+	sprite.height = options.height ?? 100;
+	sprite.color = options.color ?? Color.WHITE;
+	return sprite.texture;
 }
