62b15e9b6ebc1c552bedc33c8ffab06e25079b23
Ayfri <pierre.ayfri@gmail.com>
2021-06-14 08:40:39 +0200

    docs: Add documentation to textures related functions.

--- src/textures/TextureManager.ts
+++ src/textures/TextureManager.ts
@@ -2,15 +2,36 @@ import * as PIXI from 'pixi.js';
 import {Sprite} from '../sprites';
 import {Color} from '../utils';
 
+/**
+ * A texture or its name.
+ */
 export type TextureOrName = string | PIXI.Texture;
+/**
+ * A tuple containing the texture name and its path.
+ */
 export type TextureNameAndPath = [name: string, path: string];
+/**
+ * A record containing texture names as keys, and their paths as values.
+ */
 export type TexturesNameAndPath = Record<string, string>;
+/**
+ * A record containing textures names as keys, and themselves as values.
+ */
 export type TexturesAndName = Record<string, PIXI.Texture>;
 
+/**
+ * The list of textures loaded via the {@link loadTextures} and {@link loadTexture} functions.
+ */
 export const loadedTexturesNames: string[] = [];
 
 export async function loadTextures(texturesNamesAndPath: TexturesNameAndPath): Promise<TexturesAndName>;
 export async function loadTextures(texturesNamesAndPath: TextureNameAndPath[]): Promise<TexturesAndName>;
+/**
+ * Loads the following textures via the {@link PIXI.Loader.shared} loader.
+ *
+ * @param texturesNamesAndPath - The textures to load, as a list or a record.
+ * @returns - The loaded textures as a record.
+ */
 export async function loadTextures(texturesNamesAndPath: TextureNameAndPath[] | TexturesNameAndPath): Promise<TexturesAndName> {
 	const textures: TextureNameAndPath[] = texturesNamesAndPath instanceof Array ? texturesNamesAndPath : Object.entries(texturesNamesAndPath);
 
@@ -27,6 +48,13 @@ export async function loadTextures(texturesNamesAndPath: TextureNameAndPath[] | 
 
 export async function loadTexture(name: string, path: string): Promise<PIXI.Texture>;
 export async function loadTexture(texturesNameAndPath: TextureNameAndPath): Promise<PIXI.Texture>;
+/**
+ * Loads the following texture via the {@link PIXI.Loader.shared} loader.
+ *
+ * @param texture - The texture to load or the texture and the path/.
+ * @param path - The texture path if not put in the first argument.
+ * @returns - The loaded texture.
+ */
 export async function loadTexture(texture: TextureNameAndPath | string, path?: string): Promise<PIXI.Texture> {
 	const name = texture instanceof Array ? texture[0] : texture;
 	path ??= texture[1];
@@ -40,10 +68,24 @@ export async function loadTexture(texture: TextureNameAndPath | string, path?: s
 	return PIXI.Loader.shared.resources[name].texture!;
 }
 
+/**
+ * Get a texture from the {@link PIXI.Loader.shared} loader, must be present in the {@link loadedTexturesNames} to asserts that it has been loaded.
+ *
+ * @param name - The name of the texture to search.
+ * @returns - The resulting texture or null if not found.
+ */
 export function getTexture(name: string): PIXI.Texture | null {
 	return loadedTexturesNames.includes(name) ? PIXI.Loader.shared.resources[name].texture! : null;
 }
 
+/**
+ * Get a texture from the {@link PIXI.Loader.shared} loader, must be present in the {@link loadedTexturesNames} to asserts that it has been loaded.
+ *
+ * @remarks Throws an error if the texture was not found in the {@link loadedTexturesNames}.
+ *
+ * @param name - The name of the texture to search.
+ * @returns - The resulting texture.
+ */
 export function getTextureOrThrow(name: string): PIXI.Texture {
 	if (loadedTexturesNames.includes(name)) return PIXI.Loader.shared.resources[name].texture!;
 	else throw new Error(`Texture '${name}' not found.`);
