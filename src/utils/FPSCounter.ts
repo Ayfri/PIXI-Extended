93d7f8c271cd6bc30127a97c6914d27fbe3d6697
Ayfri <pierre.ayfri@gmail.com>
2021-06-14 16:32:09 +0200

    feat: Add updateBySeconds value in FPSCounter, add documentation.

--- src/utils/FPSCounter.ts
+++ src/utils/FPSCounter.ts
@@ -22,10 +22,25 @@ export class FPSCounter extends Text implements FPSCounterOptions {
 	public autoStart: boolean;
 	/**
 	 * What the counter will show.
+	 *
+	 * @remarks use `${count}` to represent the value.
+	 * @default 'FPS: ${count}'
 	 */
 	public presentation: string = 'FPS: ${count}';
+	/**
+	 * The priority of the FPSCounter.
+	 *
+	 * @default PIXI.UPDATE_PRIORITY.LOW
+	 */
 	public priority: PIXI.UPDATE_PRIORITY;
 	public ticker: PIXI.Ticker;
+	/**
+	 * The number of time to update the FPSCounter per second.
+	 *
+	 * @remarks Upper values means less precise values.
+	 * @default 2
+	 */
+	public updateBySeconds: number = 2;
 	private lastTime: number = Date.now();
 	private timeValues: number[] = [];
 
@@ -40,22 +55,42 @@ export class FPSCounter extends Text implements FPSCounterOptions {
 		this.ticker.add(this.update, this, this.priority);
 	}
 
+	/**
+	 * Returns the approximate value of the FPS.
+	 *
+	 * @see http://pixijs.download/dev-fix-utils-docs/docs/PIXI.Ticker_.html#FPS
+	 *
+	 * @returns - The approximate number.
+	 */
 	public get approximate(): number {
 		return this.ticker.FPS;
 	}
 
+	/**
+	 * Returns true if the FPSCounter is started.
+	 *
+	 * @returns - The value.
+	 */
 	public get started(): boolean {
 		return this.ticker.started;
 	}
 
+	/**
+	 * Returns the mean value from the last 30 frames.
+	 *
+	 * @remarks Only update 2 times per second.
+	 *
+	 * @returns - The number in a Promise because it is only available 2 times per second.
+	 */
 	public get value(): Promise<number> {
 		return new Promise<number>(resolve => {
+			const quotient = 60 / this.updateBySeconds;
 			const currentTime = Date.now();
-			this.timeValues.push(1000 / (currentTime - this.lastTime));
-			while (this.timeValues.length === 30) {
+			this.timeValues.push(1000 / (quotient * 2) / 60 / (currentTime - this.lastTime));
+			while (this.timeValues.length === quotient) {
 				const total = this.timeValues.reduce((p: number, a: number) => p + a);
 				this.timeValues = [];
-				resolve(total / 30);
+				resolve(total / quotient);
 			}
 			this.lastTime = currentTime;
 		});
@@ -70,11 +105,13 @@ export class FPSCounter extends Text implements FPSCounterOptions {
 	}
 
 	private setFPS(): void {
+		const quotient = 60 / this.updateBySeconds;
 		const currentTime = Date.now();
-		this.timeValues.push(1000 / (currentTime - this.lastTime));
-		if (this.timeValues.length === 30) {
+
+		this.timeValues.push(1000 / (quotient * 2) / 60 / (currentTime - this.lastTime));
+		if (this.timeValues.length >= quotient) {
 			const total = this.timeValues.reduce((p: number, a: number) => p + a);
-			this.text = this.presentation.replace('${count}', (total / 30).toFixed(2));
+			this.text = this.presentation.replace(/\${count}/g, (total / quotient).toFixed(2));
 			this.timeValues = [];
 		}
 		this.lastTime = currentTime;
