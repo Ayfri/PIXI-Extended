27982fdf91be44fb4264a7c9490425c1deafffef
Ayfri <pierre.ayfri@gmail.com>
2021-06-22 02:08:45 +0200

    refactor: Remove old bundle gulp task for old tests.

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,13 @@ const {exec} = require('child_process');
  */
 const esbuildOptions = {
 	bundle: true,
-	format: 'esm',
+	format: 'cjs',
 	keepNames: true,
 	minifySyntax: true,
 	minifyWhitespace: true,
-	outfile: 'bundle.js',
-	sourcemap: 'external',
-	target: ['chrome61', 'firefox57', 'safari11', 'edge18'],
+	outfile: 'index.js',
+	sourcemap: false,
+	target: ['node14'],
 };
 
 function bundle() {
@@ -31,14 +31,6 @@ function watch() {
 
 gulp.task('clean', () => {
 	return gulp.src('types', {read: false, allowEmpty: true}).pipe(clean());
-});
-
-gulp.task('publish', () => {
-	esbuildOptions.target = 'node14';
-	esbuildOptions.format = 'cjs';
-	esbuildOptions.outfile = 'index.js';
-	esbuildOptions.sourcemap = false;
-	return gulp.src('./src/index.ts').pipe(esbuild(esbuildOptions)).pipe(gulp.dest('dist'));
 });
 
 exports.bundle = bundle;
